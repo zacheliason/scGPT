a766d45a961c180832fec2c23fd9b0cedfd2cb44
zacheliason <eliasonzach@gmail.com>
2025-02-21 10:20:20 -0500

    fix tensor

--- scgpt_perturb_map.py
+++ scgpt_perturb_map.py
@@ -681,7 +681,9 @@ for epoch in range(1, epochs + 1):
     val_metrics = compute_perturbation_metrics(
         val_res, pert_data.adata[pert_data.adata.obs["condition"] == "ctrl"]
     )
-    masked_positions = torch.ones_like(val_res["truth"], dtype=torch.bool)
+    masked_positions = torch.ones_like(
+        torch.from_numpy(val_res["truth"]), dtype=torch.bool
+    )
     loss = loss_mse = criterion(val_res["pred"], val_res["truth"], masked_positions)
 
     val_losses.append(loss)  # Assuming val_metrics contains a "loss" key
